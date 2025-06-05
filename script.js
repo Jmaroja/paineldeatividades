d24c00233fa4c199546185ed011263e3f45c9850
Jr maroja <juniormaroja@gmail.comm>
2025-06-05 13:11:29 -0300

    Descrição da mudança

--- script.js
+++ script.js
@@ -159,26 +159,6 @@ let atividadesFiltradas = [];
 let calendar = null;
 let usuariosMap = {};
 
-// Inicialização do Firebase para o projeto nordil-atividades
-const firebaseConfig = {
-  apiKey: "AIzaSyBndhAbgEbFxCZvTVfkqMH6MTuyzhPTxUo",
-  authDomain: "nordil-atividades.firebaseapp.com",
-  projectId: "nordil-atividades",
-  storageBucket: "nordil-atividades.firebasestorage.app",
-  messagingSenderId: "138660523776",
-  appId: "1:138660523776:web:d47955e6b48401c233cde5"
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-
-// Agora sim pode usar db aqui embaixo
-function listarUsuariosMap() {
-  db.collection("usuarios").get().then(snapshot => {
-  });
-}
-
-
 function listarUsuariosMap() {
     db.collection('usuarios').get().then(snapshot => {
         usuariosMap = {};
